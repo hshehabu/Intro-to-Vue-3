ae19209a9844c7078f4c6237d188d9ea7b31abca
hshehabu <hamzashehabu457@gmail.com>
2024-08-23 09:39:01 +0300

    lesson 5 completed

--- main.js
+++ main.js
@@ -5,7 +5,12 @@ const app = Vue.createApp(
                 product : 'socks',
                 image : './assets/images/socks_green.jpg',
                 url : 'https://yenesuq.com',
-                inventory : 0
+                inventory : 0,
+                details: ['50% cotton', '30% wool', '20% polyester'],
+                variants : [
+                    {id : 2234 , color : "green"},
+                    {id : 3434 , color : "blue"}
+                ]
             }
         }
     }

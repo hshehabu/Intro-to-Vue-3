11afcf771d7e64ffcf231f5c472da7f3a68ca40c
hshehabu <hamzashehabu457@gmail.com>
2024-08-28 19:11:32 +0300

    quantity

--- main.js
+++ main.js
@@ -9,8 +9,8 @@ const app = Vue.createApp(
                 inventory : 5,
                 details: ['50% cotton', '30% wool', '20% polyester'],
                 variants : [
-                    {id : 2234 , color : "green" , image : './assets/images/socks_green.jpg'},
-                    {id : 3434 , color : "blue" , image : './assets/images/socks_blue.jpg'}
+                    {id : 2234 , color : "green" , image : './assets/images/socks_green.jpg', quantity : 30},
+                    {id : 3434 , color : "blue" , image : './assets/images/socks_blue.jpg' , quantity : 0}
                 ],
                 cart : 0,
                 brand : 'Vue Mastery',
